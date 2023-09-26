9d62aa7794ad98970955eebb1e3b400a43c3549d
BinhBui0912 <duybinhh03@gmail.com>
2023-09-26 09:45:23 +0700

    Source Code

---
+++ J03009 - TẬP TỪ RIÊNG CỦA HAI XÂU/J03009.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+public class J03009 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String test = sc.nextLine();
+        int t = Integer.parseInt(test);
+        while(t-- > 0){
+            String a = sc.nextLine();
+            String b = sc.nextLine();
+            TreeSet<String> set = new TreeSet<>();
+            TreeSet<String> set2 = new TreeSet<>();
+            String[] arr = b.split("\\s+");
+            for(String x : arr){
+                set.add(x);
+            }
+            String[] arr2 = a.split("\\s+");
+            for(String x : arr2){
+                if(!set.contains(x))
+                    set2.add(x);
+            }
+            for(String x : set2){
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- J03010 - ĐỊA CHỈ EMAIL/J03010.java
+++ J03010 - ĐỊA CHỈ EMAIL/J03010.java
@@ -1,5 +1,8 @@
+import java.util.HashMap;
 import java.util.Scanner;
+
 public class J03010 {
+    static HashMap<String, Integer> map = new HashMap<>();
     public static String chuanHoa(String s){
         s = s.trim().toLowerCase();
         String[] arr = s.split("\\s+");
@@ -7,6 +10,16 @@ public class J03010 {
         tmp += arr[arr.length - 1];
         for(int i = 0; i < arr.length - 1; i++){
             tmp += arr[i].substring(0, 1);
+        }
+        if(!map.containsKey(tmp)){
+            map.put(tmp, 1);
+        }
+        else{
+            int tansuat = map.get(tmp);
+            map.put(tmp, tansuat + 1);
+        }
+        if(map.get(tmp) > 1){
+            tmp += map.get(tmp).toString();
         }
         tmp += "@ptit.edu.vn";
         return tmp.trim();
